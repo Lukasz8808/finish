ca18dc52621acad832a24dd3b4b66cd0fc7a7859
Lukasz Maslaczyk <lukasz.maslaczyk@gmail.com>
2019-09-07 10:59:20 +0200

    tree implention

--- src/main/java/Tree/Tree.java
+++ src/main/java/Tree/Tree.java
@@ -1,49 +1,43 @@
-package Tree;
 
-import java.util.ArrayList;
-import java.util.List;
-
+        package Tree;
+        import java.util.ArrayList;
+        import java.util.List;
 public class Tree {
     private TreeNode root;
     private int size;
-
     public Tree(){}
     public Tree(int [] table) {
         for (int val:table)
             add(val);
     }
-
     public void remove(int value) {
-        if(root == null) return;
+        if (root == null) return;
         TreeNode nodeToDelete = findNodeByValue(root, value);
-        if(nodeToDelete == null) return;
-
+        if (nodeToDelete == null) return;
         TreeNode parentNode = findParent(nodeToDelete, root);
-
         List<TreeNode> children = new ArrayList();
         getChildren(nodeToDelete, children);
-        if(parentNode.getRight() != null && nodeToDelete.getValue() == parentNode.getRight().getValue())
+        if (parentNode.getRight() != null && nodeToDelete.getValue()== parentNode.getValue())
             parentNode.setRight(null);
-        else if(parentNode.getLeft() != null)
-            parentNode.setLeft(null);
+        else if (parentNode.getLeft() != null);
+        parentNode.setLeft(null);
         for (TreeNode node:children)
-            this.add(node.getValue());
+            this.add(node.getValue());{
+        }
     }
-
-    private TreeNode findParent(TreeNode childNode, TreeNode tmpNode) {
+    private TreeNode findParent (TreeNode childNode, TreeNode tmpNode){
         if(tmpNode == null)
             return null;
-        if(tmpNode.getLeft() != null && childNode.getValue() == tmpNode.getLeft().getValue())
+        if (tmpNode.getLeft()!= null && childNode.getValue()==tmpNode.getLeft().getValue())
             return tmpNode;
-        if(tmpNode.getRight() != null && childNode.getValue() == tmpNode.getRight().getValue())
+        if (tmpNode.getRight()!= null && childNode.getValue()==tmpNode.getRight().getValue())
             return tmpNode;
         if(childNode.getValue() < tmpNode.getValue())
             return findParent(childNode, tmpNode.getLeft());
-        if(childNode.getValue() > tmpNode.getValue())
+        if(childNode.getValue()> tmpNode.getValue())
             return findParent(childNode, tmpNode.getRight());
         return null;
     }
-
     private void getChildren(TreeNode parentNode, List children) {
         if(parentNode.getLeft() != null) {
             children.add(parentNode.getLeft());
@@ -54,14 +48,12 @@ public class Tree {
             getChildren(parentNode.getRight(), children);
         }
     }
-
     public boolean search(int value) {
         if(root == null)
             return false;
         TreeNode tmpNode = root;
         return findNodeByValue(tmpNode, value) == null ? false : true;
     }
-
     private TreeNode findNodeByValue(TreeNode tmpNode, int value) {
         if(tmpNode == null)
             return null;
@@ -73,7 +65,6 @@ public class Tree {
             return findNodeByValue(tmpNode.getRight(), value);
         return null;
     }
-
     public void add(int value) {
         TreeNode newNode = new TreeNode(value);
         TreeNode tmpNode = root;
@@ -83,7 +74,6 @@ public class Tree {
         }
         size = insertNode(newNode, tmpNode) ? ++size : size;
     }
-
     private boolean insertNode(TreeNode newNode, TreeNode tmpNode) {
         if(newNode.getValue() < tmpNode.getValue()) {
             if (tmpNode.getLeft() == null) {
